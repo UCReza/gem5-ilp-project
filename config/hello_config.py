dddef75f928729f2f8dfb9d858c9d1d8c9336d1b
Reza Shrestha <rezashrestha@Mac-27695.lan>
2025-07-26 23:24:36 -0500

    Initial commit: Add Step 1 Hello World project files

---
+++ config/hello_config.py
@@ -0,0 +1,34 @@
+# hello_run.py - A simple ARM SE mode config for gem5 v25
+
+from m5.objects import *
+from gem5.simulate.simulator import Simulator
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.isas import ISA
+from gem5.components.cachehierarchies.classic.no_cache import NoCache
+
+import os
+
+# Create the processor (TimingSimpleCPU, ARM)
+processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.ARM)
+
+# Create the board (clock, memory, cache, etc.)
+board = SimpleBoard(
+    clk_freq="1GHz",
+    processor=processor,
+    memory=SingleChannelDDR3_1600(size="512MB"),
+    cache_hierarchy=NoCache()
+)
+
+# Set the workload binary path
+binary_path = "/home/rezashrestha/Documents/MSCS-531/Week5/workload/hello_arm"
+
+# Set the process to run
+board.set_se_binary_workload(binary_path)
+
+# Set up the simulator
+simulator = Simulator(board=board)
+
+# Run the simulation
+simulator.run()
