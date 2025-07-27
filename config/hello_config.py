e9463a36efcc6df15934131207ec578e4bc70e27
Reza Shrestha <rezashrestha@Mac-27979.lan>
2025-07-27 13:04:32 -0500

    updated config file for binary error

--- config/hello_config.py
+++ config/hello_config.py
@@ -7,11 +7,16 @@ from gem5.components.processors.simple_processor import SimpleProcessor
 from gem5.components.memory import SingleChannelDDR3_1600
 from gem5.isas import ISA
 from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.processors.cpu_types import CPUTypes  
+from gem5.resources.resource import CustomResource
+
+
 
 import os
 
 # Create the processor (TimingSimpleCPU, ARM)
-processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.ARM)
+processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.ARM, num_cores=1)
+
 
 # Create the board (clock, memory, cache, etc.)
 board = SimpleBoard(
@@ -22,10 +27,9 @@ board = SimpleBoard(
 )
 
 # Set the workload binary path
-binary_path = "/home/rezashrestha/Documents/MSCS-531/Week5/workload/hello_arm"
+binary = CustomResource("/Users/rezashrestha/Documents/MSCS-531/Week5/workload/hello_arm")
+board.set_se_binary_workload(binary)
 
-# Set the process to run
-board.set_se_binary_workload(binary_path)
 
 # Set up the simulator
 simulator = Simulator(board=board)
